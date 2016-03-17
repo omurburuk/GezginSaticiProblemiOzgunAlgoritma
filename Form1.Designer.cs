86b91fc7861f4c7ed099e02336f0b4d44aaca217
Ömür Buruk <burukomur@gmail.com>
2016-03-17 18:41:15 +0200

    GezginSatıcıProblemi en kısa Yol ve toplam Yol Grafik
    GezginSatıcıProblemi en kısa Yol ve toplam Yol Grafiğinin çizilmesi
    hesaplanması dizideki her bir eleman için kontol edilen mesafe özgün
    algoritma.

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace WindowsFormsApplication5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(378, 289);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Control;
+            this.label6.Location = new System.Drawing.Point(252, 330);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(126, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Gezgin-Satıcı En Kısa Yol";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(326, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 40);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Şehir sayısını Onayla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Gidilecek Şehir Sayısı";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(171, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(60, 315);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(66, 43);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Çizim Yap";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(628, 346);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(628, 314);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(582, 346);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Y Kor :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(582, 315);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "X Kor :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 385);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace WindowsFormsApplication5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+        int cizx, cizy;
+        int[,] ilknokta = new int[1, 1];
+        Point nokta1 = new Point(0, 0);
+        Point ilk;
+        public static int digsay = 0;
+        void cizimyap()
+        {
+            int tutulanx = 0;
+            int tutulany = 0;
+            for (int i = 0; i < dizi1.Length / 2 - 1; i++)
+            {
+                for (int j = 0; j < 1; j++)
+                {
+                    int karsix = dizi1[i + 1, j];
+                    int karsiy = dizi1[i + 1, j + 1];
+                    if (digsay == 0)
+                    {
+                        ilk = new Point(dizi1[i, j], dizi1[i, j + 1]);
+                        tutulanx = dizi1[i, j];
+                        tutulany = dizi1[i, j + 1];
+                        nokta1.X = tutulanx;
+                        nokta1.Y = tutulany;
+                    }
+
+                    digsay++;
+                    Point nokta2 = new Point(karsix, karsiy);
+                    int indisx = i + 1;
+                    int indisy = j + 1;
+                    tutulanx = nokta1.X;
+                    tutulany = nokta1.Y;
+                    if (nokta1.X != -1 && nokta1.Y != -1 && nokta2.X != -1 && nokta2.Y != -1)
+                        mesafehesapla(tutulanx, tutulany, karsix, karsiy, indisx, indisy, nokta1, nokta2);
+                    else if (i == ((dizi1.Length / 2) - 2))
+                    {
+                        sayac1 = dizi1.Length / 2 - 2;
+                        mesafehesapla(tutulanx, tutulany, karsix, karsiy, indisx, indisy, nokta1, nokta2);
+
+                    }
+                    else
+                        sayac1++;
+                }
+
+            }
+
+        }
+        int enyakın = 1920;
+        int yakinindisx = 0;
+        int yakinindisy = 0;
+        int sayac1 = 0;
+        int cizilen = 0;
+        Point enyak = new Point(0, 0);
+        int s = 0;
+        int topmesafe = 0;
+        double mesafe;
+        public void mesafehesapla(int a, int b, int w, int z, int indx, int indy, Point nok1, Point nok2)
+        {
+            sayac1++;
+            int sayix = 0;
+
+            if (nok1.X != -1 && nok1.Y != -1 && nok2.X != -1 && nok2.Y != -1)
+            {
+                mesafe = Math.Sqrt(Math.Pow(w - a, 2) + Math.Pow(z - b, 2));
+                if (mesafe < enyakın)
+                {
+                    enyakın = (int)mesafe;
+                    yakinindisx = indx;
+                    yakinindisy = indy;
+                    sayix = dizi1[yakinindisx, yakinindisy];
+                    enyak.X = w;
+                    enyak.Y = z;
+                }
+            }
+            if (sayac1 == dizi1.Length / 2 - 1 && nok1 != enyak)
+            {
+
+                topmesafe += (int)mesafe;
+                sayac1 = 0;
+                dizi1[0, 0] = -1;
+                dizi1[0, 1] = -1;
+                enyakın = 1920;
+                for (int u = 0; u < 2; u++)
+                    dizi1[yakinindisx, u] = -1;
+                if (nokta1.X != -1 && nokta1.Y != -1 && enyak.X != -1 && enyak.Y != -1)
+                {
+                    g.DrawLine(pen, nok1, enyak);
+                    cizilen++;
+
+                }
+                nokta1 = enyak;
+
+                for (int k = 0; k < dizi1.Length / 2 - 1; k++)
+                {
+                    sayac1 = 0;
+                    cizimyap();
+
+                }
+                sayac1 = 0;
+                int eksisay = 0;
+                for (int y = 0; y < dizi1.Length / 2 - 1; y++)
+                    for (int t = 0; t < 2; t++)
+                    {
+                        if (dizi1[y, t] == -1)
+                            eksisay++;
+                    }
+                if (eksisay == dizi1.Length)
+                    g.DrawLine(pen, nok1, ilk);
+                else
+                {
+                    sayac1 = 0;
+                    cizimyap();
+                }
+                eksisay = 0;
+
+            }
+
+            label7.Text = "En Kısa Toplam Mesafe : " + Convert.ToString(topmesafe) + " Km";
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            button2.Visible = false;
+        }
+
+        void mesefehesapla(Rectangle x, Rectangle y)
+        {
+            label1.Text = Convert.ToString(x.Y);
+            double mesafe = Math.Sqrt(Math.Pow(x.Y - y.Y, 2) + Math.Pow(x.X - y.X, 2));
+            label1.Text = Convert.ToString(mesafe);
+        }
+
+
+        int geciciX, geciciy;
+        public static int sayac = 0;
+        Point nokta, nokta2;
+
+
+        Pen pen = new Pen(Color.Blue, 2);
+        Rectangle kare;
+        Graphics g;
+        ArrayList dizi = new ArrayList();
+
+        public static int k = 0;
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (sehirsay != 0)
+            {
+                geciciX = e.X;
+                geciciy = e.Y;
+
+
+
+                label3.Text = Convert.ToString(geciciX);
+                label4.Text = Convert.ToString(geciciy);
+                kare = new Rectangle(geciciX, geciciy, 2, 2);
+                g = this.CreateGraphics();
+                g.DrawRectangle(pen, kare);
+                dizi1[sayac, digsay] = e.X;
+                dizi1[sayac, digsay + 1] = e.Y;
+                sayac++;
+                if (sayac == dizi1.Length / 2)
+                {
+                    MessageBox.Show("Yeterli Şehir Girildi Cizim yapabilirsiniz");
+                    button2.Visible = true;
+                }
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        public static int sehirsay = 0;
+        public static int[,] dizi1;
+       
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            sehirsay = Convert.ToInt16(textBox1.Text);
+            dizi1 = new int[sehirsay, 2];
+            MessageBox.Show("Şehirleri Form Ekranına Tıklayarak Yerleştiriniz..");
+        }
+
+        private void Form1_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            if (sehirsay != 0)
+            {
+                geciciX = e.X;
+                geciciy = e.Y;
+
+
+
+                label3.Text = Convert.ToString(geciciX);
+                label4.Text = Convert.ToString(geciciy);
+                kare = new Rectangle(geciciX, geciciy, 2, 2);
+                g = this.CreateGraphics();
+                g.DrawRectangle(pen, kare);
+                dizi1[sayac, digsay] = e.X;
+                dizi1[sayac, digsay + 1] = e.Y;
+                sayac++;
+                if (sayac == dizi1.Length / 2)
+                {
+                    MessageBox.Show("Yeterli Şehir Girildi Cizim yapabilirsiniz");
+                    button2.Visible = true;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cizimyap();
+        }
+    }
+}
